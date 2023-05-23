c64a68b2a770eff4564775539c3fe83911019620
nina <128232339+ninalor02@users.noreply.github.com>
2023-05-23 18:13:57 -0500

    first commit for JS

---
+++ assets/scripts/script.js
@@ -0,0 +1,26 @@
+/* VAR BODY - DOCUMENT BODY */
+var header = document.querySelector("header");
+var score = document.getElementById("score");
+var submitButton = document.getElementById("submitButton");
+
+var question1Header = document.getElementById("question1Header");
+var choiceA = document.getElementById("A");
+var choiceA = document.getElementById("B");
+var choiceA = document.getElementById("C");
+var choiceA = document.getElementById("D");
+var answerResponse = document.getElementById("answerResponse");
+
+
+
+/* QUESTION ARRAY*/
+
+var question1 = [
+    {
+        "question1" : "WHat is the world's most popular language?",
+        "A" : "A. English",
+        "B" : "B. Spanish",
+        "C" : "C. French",
+        "D" : "D. Javascript",
+    }
+]
+console.log();
