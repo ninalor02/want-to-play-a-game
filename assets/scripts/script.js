6d2b5ec2d8f96bcaca730ac3f687746a26e20c6b
nina <128232339+ninalor02@users.noreply.github.com>
2023-05-31 16:24:04 -0500

    adding html page, fixing css page

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,17 +5,18 @@ var secondsLeft =10;
 
 function startTime() {
     //seting interval in variable //
-    const today = new Date() {
-        let s = today.getSeconds();
-        document.getElementById('set').innerHTML = h + ":" + s;
-        setTimeout(startTime, 1000);
+const today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+  m = checkTime(m);
     }
 function checkTime(i) {
     if(i < 10) {i = "0" + i};
     return i;
 };
 
-}
+
   
 
 /* I am unsure of how to set the timers and set it up and setting up the scores*/
