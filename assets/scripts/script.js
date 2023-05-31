2b25c1cc9b0d206e2868348bbee15d9b6816f5cf
nina <128232339+ninalor02@users.noreply.github.com>
2023-05-30 21:46:25 -0500

    updating js

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,21 +1,21 @@
 /* VAR BODY - DOCUMENT BODY */
+
 var mainE1 = document.getElementById('time');
 var secondsLeft =10;
 
-function setTime() {
+function startTime() {
     //seting interval in variable //
-    var timerInterval = setInterval(function() {
-        secondsLeft--;
-        timerInterval.textcontent = secondsLeft + "seconds left till quiz end"
-    
-        if(secondsLeft ===0){
-            clearInterval(timerInterval);
+    const today = new Date() {
+        let s = today.getSeconds();
+        document.getElementById('set').innerHTML = h + ":" + s;
+        setTimeout(startTime, 1000);
+    }
+function checkTime(i) {
+    if(i < 10) {i = "0" + i};
+    return i;
+};
 
-            sendMessage();
-        }
-    
-    }, 1000);
 }
-setTime();
+  
 
 /* I am unsure of how to set the timers and set it up and setting up the scores*/
