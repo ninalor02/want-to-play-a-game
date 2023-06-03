5b86e994d1af42ec37a710cab8e62d09b8e575ad
nina <128232339+ninalor02@users.noreply.github.com>
2023-06-02 23:04:28 -0500

    updating html and JS

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,24 +1,106 @@
 /* VAR BODY - DOCUMENT BODY */
+var timerElement = document.querySelector(".time");
+var timerElement = document.querySelector(".timer-count");
+var startButton = document.querySelector(".start-button");
+console.log('timer1'); /* this is working in console.log*/ 
 
-var timerEl = document.getElementById("timer");
-var mainEl = document.querySelector("main");
- console.log('timer');
- console.log('main');
+
+var correctAnswer = "";
+var isWin = false;
+var timer;
+var timerCount;
+
+var correctAnswer = [];
+console.log('testing words1'); /* this is working in console.log*/ 
+
+var words = ["J.K. Rowling","Voldemort", "Stone", "Severus Snape", "Happy Potter", "E.L. James", "Maya Banks", "James Patterson", "Dumboldore", "Hagrid", "Diary", "Basilisk", "Ring", "Draco Malfoy", "Ronald Weasley"];
+
+ console.log('timer2'); /* this is working in console.log*/ 
+
+ var secondsLeft = 60;
+ console.log('testing2'); /* this is working in console.log*/ 
+
+ function init() {
+  getTimer();
+}
+console.log('false'); /* this is working in console.log*/ 
+
+
+function startGame() {
+  isWin = false;
+  console.log(); /* this is NOT working in console.log*/ 
+  timerCount = 60;
+  console.log('data'); /* this is NOT working in console.log*/ 
+
+  startButton.disabled = true;
+  renderWords()
+  startTimer()
+  console.log('data'); /* this is NOT working in console.log*/ 
+}
+
+function startTimer() {
+  // Sets timer
+  timer = setInterval(function() {
+    timerCount--;
+    timerElement.textContent = timerCount;
+    if (timerCount >= 0) {
+      // Tests if win condition is met
+      if (isWin && timerCount > 0) {
+        // Clears interval and stops timer
+        clearInterval(timer);
+        winGame();
+
+        console.log('tesing3');  /* this is NOT working in console.log*/ 
+      }
+    }
+    // Tests if time has run out
+    if (timerCount === 0) {
+      // Clears interval
+      clearInterval(timer);
+      loseGame();
+    }
+  }, 1000);
+
+  console.log('tesing4'); /* this is NOT working in console.log*/ 
+}
+
+
+
+
+
+
+
+
+
+
+
+/*
 
  function setTime() {
 
-var timeLeft = 60;
-console.log('timeLeft');
-
   var timerInterval = setInterval(function() {
     secondsLeft--;
-    timer.textContent = secondsLeft + "seconds left";
-    console.log('timerInterval', timerInterval);
+    timeEl.textContent = secondsLeft + "running out of time";
+    
+    console.log('timerInterval'); /* this is NOT working in console.log*/ 
+/*
+    if(secondsLeft > 1) {
 
-    if(secondsLeft === 0) {
+      timerEl.textContent = secondsLeft + 'seconds remaining';
+      secondsLeft--;
+    } else if (secondsLeft === 1) {
+      timeEl.textContent + 'seconds remaining';
+      secondsLeft--;
+    } else {
+      timeEl.textContent = '';
+
       clearInterval(timerInterval);
-      sendMessage('Times Up');
+
+      sendMessage('out of time');
+      console.log('out of time'); /* this is NOT working in console.log 
     }
 
   }, 1000);
-}
+}   
+
+*/
