1cec4afe69fda7b365b5c66abc508c3f90c6b385
nina <128232339+ninalor02@users.noreply.github.com>
2023-06-08 17:35:57 -0500

    added all Q's in JS

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -6,3 +6,58 @@ var score = scoreIdCounter = 0;
 var highscore = [];
 var timer = setInterval(countdown, 1000);
 
+var questions = [
+// question 1
+  {
+  question: "Who is the author for the book series Harry Potter?",
+  options: [
+    "a. E.L. James",
+    "b. J.K. Rowling",
+    "c. Maya Banks",
+    "d. James Patterson",
+    console.log('testing', b. J.K. Rowling)
+  ],
+  answer: "b. J.K. Rowling",
+},
+
+// question 2
+{
+  question: "Who is Tom Riddle",
+  options: [
+    "a. Voldemort",
+    "b. Dumboldore",
+    "c. Hagrid",
+    "d. Severus Snape",
+    console.log('testing2',a. Voldemort )
+  ],
+  answer: "a. Voldemort",
+},
+
+// question 3
+{
+  question: "Which of the following that was not a Horcrux?",
+  options: [
+    "a. Diary",
+    "b. Basilisk",
+    "c. Stone",
+    "d. Ring",
+    console.log('testing3', stone)
+  ],
+  answer: "c. Stone",
+},
+
+// question 4
+{
+  question: "Who is the half blood prince?",
+  options: [
+    "a. Happy Potter",
+    "b. Severus Snape",
+    "c. Draco Malfoy",
+    "d. Albus Dumboldore",
+    console.log('testing4')
+  ],
+  answer: "b. Severus Snape",
+},
+
+
+]
