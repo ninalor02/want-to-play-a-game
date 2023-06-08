44e80e4f7824708b0f5bd31f9c830106e1d9f9a7
nina <128232339+ninalor02@users.noreply.github.com>
2023-06-08 17:21:14 -0500

    added variables in js

---
+++ assets/scripts/script.js
@@ -0,0 +1,8 @@
+var mainContainer = document.querySelector('#main-content');
+var i = 0;
+var time = 60;
+var score = 0;
+var score = scoreIdCounter = 0;
+var highscore = [];
+var timer = setInterval(countdown, 1000);
+
