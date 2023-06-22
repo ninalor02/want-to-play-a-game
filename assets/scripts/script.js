002fd0864e4acbec78b34d68e5ad05ba4d58c2ef
nina <128232339+ninalor02@users.noreply.github.com>
2023-06-21 21:05:43 -0500

    add questions into js

---
+++ assets/scripts/script.js
@@ -0,0 +1,31 @@
+let timeLeftEl = document.getElementById("time-left");
+let startGameEl = document.getElementById("start-game");
+
+//Quiz quizQuestions object
+let quizQuestions = [
+    {
+        question: "Question 1. Who is the author for the book series Harry Potter?",
+        options: ['E.L. James', 'J.K. Rowling', 'Maya Banks', 'James Patterson'],
+        answer: "author = ['E.L. James']"  
+    },
+    {
+        question: "Question 2. Who is Tom Riddle?",
+        options: ["Voldemort", "Albus Dumboldore", "Professor Quill", "Severus Snape"],
+        answer: "Voldemort" 
+    },
+    {
+        question: "Which of the following that was not a Horcrux?",
+        options: ["Diary", "Basilisk", "Stone", "Ring"],
+        answer: "Stone"
+    },
+    {
+        question: "Who is the half blood prince?",
+        options: ["Harry Potter", "Severus Snape", "Voldemort", "Draco Malfoy"],
+        answer: "Severus Snape"
+    },
+     {
+        question: "Who disarmed the Elder Wand from Dumbledore?",
+        options: ["Harry Potter", "Voldemort", "Lucius Malfoy", "Draco Malfoy"],
+        answer: "Draco Malfoy"
+     }
+];
